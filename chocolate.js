3e9c97518372383d23f3ebdc262119e65608a314
Noel Biju Daniel <167795322+n3dgit2003@users.noreply.github.com>
2024-10-05 07:54:14 +0530

    Add files via upload

---
+++ chocolate.js
@@ -0,0 +1,18 @@
+function moveZerosToEnd(arr) {
+    let count = 0; 
+
+        for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 0) {
+            arr[count++] = arr[i]; 
+        }
+    }
+
+  
+    while (count < arr.length) {
+        arr[count++] = 0;
+    }
+
+    return arr;
+}
+let arr = [4, 5, 0, 1, 9, 0, 5, 0];
+console.log(moveZerosToEnd(arr)); 
